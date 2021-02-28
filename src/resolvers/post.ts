094f6ed300aea15bbbbe6462d95ec4e1976c5f47
tom <tomxu@udel.edu>
2021-02-28 13:26:00 -0500

    add CRUD single post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,20 @@
-import { Resolver, Query, Ctx } from 'type-graphql';
+import { Resolver, Query, Ctx, Arg, Int } from 'type-graphql';
 import { Post } from '../entities/Post';
 import { Mycontext } from '../types';
+import { idText } from 'typescript';
 
 @Resolver()
 export class PostResolver {
     @Query(() => [Post])
     posts(@Ctx() { em }: Mycontext): Promise<Post[]> {
         return em.find(Post, {});
+    }
+
+    @Query(() => Post, {nullable: true})
+    post(
+        @Arg('id', () => Int) id: number,
+        @Ctx() { em }: Mycontext
+    ): Promise<Post | null> {
+        return em.findOne(Post, {id});
     }
 }
